6836111de9dfc00c505245d69b529fe191711a0e
tangyx <tangyx@live.com>
2020-11-23 13:53:29 +0800

    扫码

--- imkit/src/main/java/com/mwim/qcloud/tim/uikit/business/barcodescanner/camera/DisplayConfiguration.java
+++ imkit/src/main/java/com/mwim/qcloud/tim/uikit/business/barcodescanner/camera/DisplayConfiguration.java
@@ -11,7 +11,6 @@ import java.util.List;
  *
  */
 public class DisplayConfiguration {
-    private static final String TAG = DisplayConfiguration.class.getSimpleName();
 
     private Size viewfinderSize;
     private int rotation;
